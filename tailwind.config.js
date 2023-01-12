c0b4c47b3b9408baafd9715f6934c1454981ed94
Nicolas Wallet <nicolaswallet@hotmail.fr>
2023-01-12 23:02:29 +0100

    initial version

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#e5af48",
+        "secondary": "#1c162b",
+      },
+    },
+  },
+  plugins: [],
+}
